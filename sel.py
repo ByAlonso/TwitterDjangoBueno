1091f4c25622a705300d66b0f5b3bb2e803765c9
villa <villasante.carlos@outlook.com>
2019-04-25 15:11:58 +0200

    first test with geckodriver

--- sel.py
+++ sel.py
@@ -1,4 +1,5 @@
 import unittest
+from selenium import common
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
@@ -6,16 +7,37 @@ class PythonTwitterWeb(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox(executable_path=r'C:\Users\villa\Downloads\gecko\geckodriver.exe')
         self.driver.get('http://127.0.0.1:9000/')
+        self.username_element = self.driver.find_element_by_name("username")
+        self.button_element_reset = self.driver.find_element_by_xpath('/html/body/form[2]/input[2]')
+
 
     def test_simple_search(self):
-        usernameElement = self.driver.find_element_by_name("username")
-        usernameElement.clear()
-        usernameElement.send_keys("@_lalusi")
-        usernameElement.submit()
+        self.username_element.clear()
+        self.username_element.send_keys("@realDonaldTrump")
+        self. username_element.submit()
         lista = self.driver.find_element_by_id("id_list")
-        correct_list = [['prueba', 5], ['práctica', 3], ['duni', 3], ['tweet', 3], ['王', 3], ['ahora', 2], ['quiero', 2], ['practica', 2], ['ensayo', 2], ['buena', 2], ['cama', 2], ['184', 2], ['iba', 2], ['venido', 2], ['parejas', 2], ['3', 2], ['ayer', 2], ['salir', 2], ['feliz', 2], ['muertos', 2]]
-        self.assert_(correct_list,lista)
+        self.assertIsNotNone(lista)
         self.driver.quit()
+
+    def test_reset(self):
+        self.username_element.clear()
+        self.username_element.send_keys("@realDonaldTrump")
+        self.username_element.submit()
+        self.button_element_reset.click()
+        with self.assertRaises(common.exceptions.NoSuchElementException):
+            self.driver.find_element_by_id("id_list")
+
+    def test_invalid_username(self):
+        self.username_element.clear()
+        self.username_element.send_keys("@realDonaldTrump")
+        self.username_element.submit()
+        with self.assertRaises(common.exceptions.NoSuchElementException):
+            self.driver.find_element_by_id("id_list")
+
+    def test_empty_submit(self):
+        self.button_element_reset.click()
+        self.username_element.send_keys("")
+        self.username_element.submit()
 
 if __name__ == '__main__':
     unittest.main()
