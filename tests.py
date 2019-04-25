616fc75517f1dd6f24b886b4b265cc9a40bf77a0
villa <villasante.carlos@outlook.com>
2019-04-25 15:26:40 +0200

    4 tests with geckodriver

--- tests.py
+++ tests.py
@@ -3,13 +3,13 @@ from selenium import common
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+
 class PythonTwitterWeb(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox(executable_path=r'C:\Users\villa\Downloads\gecko\geckodriver.exe')
         self.driver.get('http://127.0.0.1:9000/')
         self.username_element = self.driver.find_element_by_name("username")
         self.button_element_reset = self.driver.find_element_by_xpath('/html/body/form[2]/input[2]')
-
 
     def test_simple_search(self):
         self.username_element.clear()
@@ -26,6 +26,7 @@ class PythonTwitterWeb(unittest.TestCase):
         self.button_element_reset.click()
         with self.assertRaises(common.exceptions.NoSuchElementException):
             self.driver.find_element_by_id("id_list")
+        self.driver.quit()
 
     def test_invalid_username(self):
         self.username_element.clear()
@@ -33,11 +34,16 @@ class PythonTwitterWeb(unittest.TestCase):
         self.username_element.submit()
         with self.assertRaises(common.exceptions.NoSuchElementException):
             self.driver.find_element_by_id("id_list")
+        self.driver.quit()
 
     def test_empty_submit(self):
         self.button_element_reset.click()
-        self.username_element.send_keys("")
-        self.username_element.submit()
+        button_element_execute = self.driver.find_element_by_xpath('/html/body/form[1]/div/div[2]/input')
+        button_element_execute.click()
+        with self.assertRaises(common.exceptions.NoSuchElementException):
+            self.driver.find_element_by_id("id_list")
+        self.driver.quit()
+
 
 if __name__ == '__main__':
     unittest.main()
