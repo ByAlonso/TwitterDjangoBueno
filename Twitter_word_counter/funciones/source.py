c360333c91c9be590457a4226e6818aeabfcf78a
Lucía Montesinos <lucia.montesinos@live.u-tad.com>
2019-04-25 20:58:18 +0200

    Final

--- Twitter_word_counter/funciones/source.py
+++ Twitter_word_counter/funciones/source.py
@@ -2,6 +2,44 @@ import re
 from stop_words import get_stop_words
 import twitter
 
+
+# VARIABLES DE ENTORNO
+'''CONSUMER_KEY = os.environ['CONSUMER_KEY']
+CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
+ACCESS_TOKEN_KEY = os.environ['ACCESS_TOKEN_KEY']
+ACCESS_TOKEN_SECRET = os.environ['ACCESS_TOKEN_SECRET']'''
+
+
+class twitter_things:
+    def __init__(self,username):
+        self.username = username
+
+    def twitter_tweets(self,mock):
+        api = twitter.Api(consumer_key='xJBNVbtmRJrd44fslR38GiJNr',
+                            consumer_secret='w22znbElOOjVs2oYORs2hqxmUF1K6MfJ3YUEoefttZVDG5SbwW',
+                            access_token_key='831510109629071361-OFLx61XL1iiFQNDvH1OeBYgBWZn5dQR',
+                            access_token_secret='l3NObDqc4psYpok3YSv7HFKwuVEAdijABoE0WSE3CEfDT')
+
+        return api.GetUserTimeline(screen_name='@_lalusi', count=200, exclude_replies=True, include_rts=False)[
+               0:50]
+
+    def twitter_word_count(self, twits):
+        twits.iteratess()
+
+    def iteratess(self):
+        array = ''
+        for tweet in self:
+            exclude = []
+            for url in tweet.urls:
+                exclude.append(url.url)
+            if tweet.media is not None:
+                for media in tweet.media:
+                    exclude.append(media.url)
+            aux = tweet.text.split()
+            array = array + ' '.join([i for i in aux if i not in exclude]) + ' '
+        return word_counter(array)[0:20]
+
+
 def word_counter(array):
     acceptable_languages = ['arabic', 'bulgarian', 'catalan', 'czech', 'danish', 'dutch', 'english', 'finnish',
                             'french', 'german', 'hungarian', 'indonesian', 'italian',
@@ -46,13 +84,7 @@ def word_counter(array):
 
     return sorted(final_list, key=lambda x: x[1], reverse=True)
 
-
-'''CONSUMER_KEY = os.environ['CONSUMER_KEY']
-CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
-ACCESS_TOKEN_KEY = os.environ['ACCESS_TOKEN_KEY']
-ACCESS_TOKEN_SECRET = os.environ['ACCESS_TOKEN_SECRET']'''
-
-class Twitter2:
+class twitter2:
 
     def __init__(self,username):
         self.username = username
